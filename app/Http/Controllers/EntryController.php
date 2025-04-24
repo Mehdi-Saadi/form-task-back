bb615b7c8d7091b139e27ff2e2d3d2deeb87a605
Mehdi <mehdi.0.saadi@gmail.com>
2025-04-24 15:40:55 +0330

    save photos to public dir

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EntryController extends Controller
 {
@@ -47,10 +48,14 @@ class EntryController extends Controller
             'photo' => 'required|image|max:2048',
         ]);
 
-        $path = $request->file('photo')->store('entries', 'public');
+        $file = $request->file('photo');
+        $pictureName = Str::uuid()->toString().'.'.$file->getClientOriginalExtension();
+        $file->move(public_path('entry'), $pictureName);
+        $path = $file->path();
 
         return response()->json([
-            'url' => asset("storage/$path"),
+            'name' => $pictureName,
+            'url' => "/entry/$pictureName",
             'path' => $path,
         ]);
     }
