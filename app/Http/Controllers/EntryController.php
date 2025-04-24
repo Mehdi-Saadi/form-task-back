b61810b2dd815b58bed507913a5b53f998b8ad26
Mehdi <mehdi.0.saadi@gmail.com>
2025-04-24 14:41:03 +0330

    implement destroyPhoto and store methods

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entry;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class EntryController extends Controller
@@ -16,14 +19,29 @@ class EntryController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store or Update new resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => ['required', 'max:255'],
+            'description' => ['required'],
+            'date' => ['required', 'date'],
+            'photos' => ['required', 'array'],
+        ]);
+
+        $firstItem = Entry::first('*');
+
+        if ($firstItem) {
+            $firstItem->update($validated);
+        } else {
+            $firstItem = Entry::create($validated);
+        }
+
+        return $firstItem;
     }
 
-    public function uploadPhoto(Request $request)
+    public function uploadPhoto(Request $request): JsonResponse
     {
         $request->validate([
             'photo' => 'required|image|max:2048',
@@ -37,17 +55,14 @@ class EntryController extends Controller
         ]);
     }
 
-    public function destroyPhoto(Request $request)
+    public function destroyPhoto(Request $request): Response
     {
-        $request->validate([
-            'photo' => 'required|image|max:2048',
-        ]);
+        $pathToDelete = $request->input('path');
 
-        $path = $request->file('photo')->store('entries', 'public');
+        if (Storage::disk('public')->exists($pathToDelete)) {
+            Storage::disk('public')->delete($pathToDelete);
+        }
 
-        return response()->json([
-            'url' => asset("storage/$path"),
-            'path' => $path,
-        ]);
+        return response()->noContent();
     }
 }
